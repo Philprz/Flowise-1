66d358b25daab9a76f1a8fbb6c57b68f48d39827
Philprz <p.perez@it-spirit.com>
2024-06-24 11:16:04 +0200

    Add script to install pip and dependencies

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Télécharger le script get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+# Installer pip
+python get-pip.py
+
 # Installer les packages nécessaires
 pip install requests beautifulsoup4
 
